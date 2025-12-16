6f6c99faaf8f254ba0713904597f10668457fbaa
YanqiuLi05 <Liy905@newschool.edu>
2025-12-16 03:32:04 -0500

    Update Eleventy config and site content

--- eleventy.config.js
+++ eleventy.config.js
@@ -3,15 +3,18 @@ import { HtmlBasePlugin } from "@11ty/eleventy";
 export default function (eleventyConfig) {
   eleventyConfig.addPlugin(HtmlBasePlugin);
 
-
-  eleventyConfig.setInputDirectory("source");
-
-
   eleventyConfig.addPassthroughCopy("source/style.css");
-  eleventyConfig.addPassthroughCopy("source/**/*.css");
-  eleventyConfig.addPassthroughCopy("source/**/*.js");
   eleventyConfig.addPassthroughCopy("source/images");
-
+  eleventyConfig.addPassthroughCopy("source/**/*.js");
+  eleventyConfig.addPassthroughCopy("source/**/*.css");
 
   eleventyConfig.addGlobalData("layout", "base.html");
+
+  return {
+    dir: {
+      input: "source",
+      includes: "_includes",
+      output: "_site"
+    }
+  };
 }
